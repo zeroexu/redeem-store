275d7cb20915d40273554598b43738753b665edc
Ali Lopez <zero.exu@gmail.com>
2019-11-28 23:21:24 -0300

    Updates

---
+++ precache-manifest.4ad6569b842045ec835c81f99529c494.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b73dc05fbd5c23c02625a5541ade575",
+    "url": "/redeem-store/index.html"
+  },
+  {
+    "revision": "02f473b4e3ccb52601c5",
+    "url": "/redeem-store/static/css/main.88ea5729.chunk.css"
+  },
+  {
+    "revision": "d763346d1b7c825acb00",
+    "url": "/redeem-store/static/js/2.ea03338a.chunk.js"
+  },
+  {
+    "revision": "02f473b4e3ccb52601c5",
+    "url": "/redeem-store/static/js/main.506bef07.chunk.js"
+  },
+  {
+    "revision": "6dce1d0ef857b551b284",
+    "url": "/redeem-store/static/js/runtime-main.07bb243b.js"
+  },
+  {
+    "revision": "2e6efe58997cf9953ce94c4829d49f8e",
+    "url": "/redeem-store/static/media/arrow-left.2e6efe58.svg"
+  },
+  {
+    "revision": "bedef3832219792f30be7f676dffa61a",
+    "url": "/redeem-store/static/media/arrow-right.bedef383.svg"
+  },
+  {
+    "revision": "2f11a957b72ff4a5c46227027cf46115",
+    "url": "/redeem-store/static/media/buy-blue.2f11a957.svg"
+  },
+  {
+    "revision": "40bc281346af2ba434a9d9792491715d",
+    "url": "/redeem-store/static/media/buy-white.40bc2813.svg"
+  },
+  {
+    "revision": "4669120cae147258c9fc868719b1d3ca",
+    "url": "/redeem-store/static/media/coin.4669120c.svg"
+  },
+  {
+    "revision": "f0b98264ce8c93768fbf37c263885aaa",
+    "url": "/redeem-store/static/media/header-x1.f0b98264.png"
+  },
+  {
+    "revision": "71af6f4be21b71f7b37a986aca3a5af6",
+    "url": "/redeem-store/static/media/logo.71af6f4b.svg"
+  }
+]);
